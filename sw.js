905700b45cbb98706b0531d0d160e3620c0a2fa8
Fan-Lin <150505581+Fan-Lin@users.noreply.github.com>
2026-06-06 01:10:18 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,23 +1,14 @@
-const CACHE = 'wf-v2';
-const ASSETS = ['./'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS))
-    .then(() => self.skipWaiting())
-  );
+const CACHE='wf-v3';
+const ASSETS=['./'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(keys=>
+    Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))
+  ).then(()=>self.clients.claim()));
 });
-
-self.addEventListener('fetch', e => {
-  if (e.request.mode === 'navigate') {
-    e.respondWith(fetch(e.request).catch(() => caches.match('./')));
-  }
+self.addEventListener('fetch',e=>{
+  if(e.request.mode==='navigate')
+    e.respondWith(fetch(e.request).catch(()=>caches.match('./')));
 });
