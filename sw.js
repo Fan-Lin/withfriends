a2432ce2c664a135d9a6e178dbf6a9d36bfc2130
Fan-Lin <150505581+Fan-Lin@users.noreply.github.com>
2026-06-05 20:31:37 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE = 'wf-v1';
+const ASSETS = ['./'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.mode === 'navigate') {
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match('./'))
+    );
+  }
+});
